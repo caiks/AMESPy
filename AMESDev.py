359b6b5c0f03d2d10ab66d5944bcf36839183be2
Cliff McCartney <caiks@greenlake.co.uk>
2019-03-22 15:13:23 +0000

    Add files via upload

--- AMESDev.py
+++ AMESDev.py
@@ -7,6 +7,44 @@ def aahr(uu,aa):
 def decomperIO(uu,vv,hr,wmax,lmax,xmax,omax,bmax,mmax,umax,pmax,fmax,mult,seed):
     return parametersSystemsHistoryRepasDecomperMaxRollByMExcludedSelfHighestFmaxIORepa(wmax,lmax,xmax,omax,bmax,mmax,umax,pmax,fmax,mult,seed,uu,vv,hr)
 
+# isOrd :: System -> Variable -> Bool
+
+def isOrd(uu,v):
+	def isOrdVal(u):
+		if u._cl == 2 or u._cl == 3:
+			return True
+		return False 
+	uat = systemsVarsSetValue
+	return any(map(isOrdVal,uat(uu,v)))
+
+# bucket :: Int -> Histogram -> Variable -> Map.Map Value Value
+
+def bucket(b,aa,v):
+	def slice(k,ll):
+		if len(ll) == 0:
+			return []
+		elif len(ll) < k:
+			return [ll] 
+		return [ll[:k]] + slice(k,ll[k:])
+	ll = sorted([ssll(ss)[0][1] for (_,ss) in hhll(aahh(red(aa,sset([v]))))])
+	return sdict(reversed([(i,j) for jj in slice(len(ll)//b,ll) for (i,j) in zip(jj,[jj[-1]]*len(jj))]))
+
+# reframeb :: Histogram ->  Map.Map Variable (Map.Map Value Value) -> Histogram
+
+def reframeb(aa,xx):
+	def repl(ss,xx):
+		ll = []
+		for (v,u) in ssll(ss):
+			(w,ww) = (v,sdict())
+			if v in xx:
+				(w,ww) = xx[v]
+			if u in ww:
+				ll.append((w,ww[u]))
+			else:
+				ll.append((w,u))
+		return llss(ll)
+	return llaa([(repl(ss,xx),q) for (ss,q) in aall(aa)])
+
 # amesIO :: IO (System, Histogram, Histogram)
 
 def amesIO():
@@ -15,7 +53,7 @@ def amesIO():
 			try:
 				return ValInt(int(s))
 			except ValueError:
-				return ValStr(s)
+				return ValStr("null")
 		return ValStr("null")
 	vv = []
 	ll = []
@@ -25,44 +63,8 @@ def amesIO():
 			vv = list(map(VarStr,rr))
 		else:
 			ss = list(map(ValStr,rr))
-			ss[0] = intornull(rr[0])
-			ss[1] = intornull(rr[1])
-			ss[3] = intornull(rr[3])
-			ss[4] = intornull(rr[4])
-			ss[17] = intornull(rr[17])
-			ss[18] = intornull(rr[18])
-			ss[19] = intornull(rr[19])
-			ss[20] = intornull(rr[20])
-			ss[26] = intornull(rr[26])
-			ss[34] = intornull(rr[34])
-			ss[36] = intornull(rr[36])
-			ss[37] = intornull(rr[37])
-			ss[38] = intornull(rr[38])
-			ss[43] = intornull(rr[43])
-			ss[44] = intornull(rr[44])
-			ss[45] = intornull(rr[45])
-			ss[46] = intornull(rr[46])
-			ss[47] = intornull(rr[47])
-			ss[48] = intornull(rr[48])
-			ss[49] = intornull(rr[49])
-			ss[50] = intornull(rr[50])
-			ss[51] = intornull(rr[51])
-			ss[52] = intornull(rr[52])
-			ss[54] = intornull(rr[54])
-			ss[56] = intornull(rr[56])
-			ss[59] = intornull(rr[59])
-			ss[61] = intornull(rr[61])
-			ss[62] = intornull(rr[62])
-			ss[66] = intornull(rr[66])
-			ss[67] = intornull(rr[67])
-			ss[68] = intornull(rr[68])
-			ss[69] = intornull(rr[69])
-			ss[70] = intornull(rr[70])
-			ss[71] = intornull(rr[71])
-			ss[75] = intornull(rr[75])
-			ss[76] = intornull(rr[76])
-			ss[77] = intornull(rr[77])
-			ss[80] = intornull(rr[80])
+			for i in [0,1,3,4,17,18,19,20,26,34,36,37,38,43,44,45,46,47,48,49,50,51,52,54,56,59,61,62,66,67,68,69,70,71,75,76,77,80]:
+				ss[i] = intornull(rr[i])
 			ll.append((llss(list(zip(vv,ss))),1))
 	aatr = llaa(ll)
 	vv = []
@@ -73,44 +75,35 @@ def amesIO():
 			vv = list(map(VarStr,rr))
 		else:
 			ss = list(map(ValStr,rr))
-			ss[0] = intornull(rr[0])
-			ss[1] = intornull(rr[1])
-			ss[3] = intornull(rr[3])
-			ss[4] = intornull(rr[4])
-			ss[17] = intornull(rr[17])
-			ss[18] = intornull(rr[18])
-			ss[19] = intornull(rr[19])
-			ss[20] = intornull(rr[20])
-			ss[26] = intornull(rr[26])
-			ss[34] = intornull(rr[34])
-			ss[36] = intornull(rr[36])
-			ss[37] = intornull(rr[37])
-			ss[38] = intornull(rr[38])
-			ss[43] = intornull(rr[43])
-			ss[44] = intornull(rr[44])
-			ss[45] = intornull(rr[45])
-			ss[46] = intornull(rr[46])
-			ss[47] = intornull(rr[47])
-			ss[48] = intornull(rr[48])
-			ss[49] = intornull(rr[49])
-			ss[50] = intornull(rr[50])
-			ss[51] = intornull(rr[51])
-			ss[52] = intornull(rr[52])
-			ss[54] = intornull(rr[54])
-			ss[56] = intornull(rr[56])
-			ss[59] = intornull(rr[59])
-			ss[61] = intornull(rr[61])
-			ss[62] = intornull(rr[62])
-			ss[66] = intornull(rr[66])
-			ss[67] = intornull(rr[67])
-			ss[68] = intornull(rr[68])
-			ss[69] = intornull(rr[69])
-			ss[70] = intornull(rr[70])
-			ss[71] = intornull(rr[71])
-			ss[75] = intornull(rr[75])
-			ss[76] = intornull(rr[76])
-			ss[77] = intornull(rr[77])
+			for i in [0,1,3,4,17,18,19,20,26,34,36,37,38,43,44,45,46,47,48,49,50,51,52,54,56,59,61,62,66,67,68,69,70,71,75,76,77]:
+				ss[i] = intornull(rr[i])
 			ll.append((llss(list(zip(vv,ss))),1))
 	aate = llaa(ll)
 	return (uunion(sys(aatr),sys(aate)),aatr,aate)
+
+# amesBucketedIO :: Int -> IO (System, Histogram, Histogram)
+
+def amesBucketedIO(b):
+	(uu,aatr,aate) = amesIO()
+	vv = uvars(uu) - sset([VarStr("Id")])
+	vvl = sset([VarStr("SalePrice")])
+	vvk = vv - vvl
+	aa = add(red(aatr,vvk),red(aate,vvk))
+	vvo = sset([w for w in vv for u in [vol(uu,sset([w]))] if isOrd(uu,w) if u > 16])
+	vvoz = sset([w for w in vvk & vvo for rr in [unit(sset([llss([(w,ValInt(0))])]))] for bb in [mul(red(aatr,sset([w])),rr)] if size(bb) > 100])
+	xx = sdict()
+	for v in vvk & (vvo - vvoz):
+		xx[v] = (VarStr(str(v)+"B"),bucket(20,aa,v))
+	xx[VarStr("SalePrice")] = (VarStr("SalePrice"+"B"),bucket(20,aatr,VarStr("SalePrice")))
+	for v in vvk & vvoz:
+		rr = unit(sset([llss([(v,ValInt(0))])]))
+		bb = mul(red(aa,sset([v])),rr)
+		aa1 = trim(sub(red(aa,sset([v])),bb))
+		xx[v] = (VarStr(str(v)+"B"),bucket(20,aa1,v))
+	aab = reframeb(aa,xx)
+	aatrb = reframeb(aatr,xx)
+	aateb = reframeb(aate,xx)
+	uub = uunion(sys(aab),uunion(sys(aatrb),sys(aateb)))
+	return (uub,aab,aatrb,aateb)
+
 
